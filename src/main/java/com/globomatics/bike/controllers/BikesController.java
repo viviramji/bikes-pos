211a949f004c243cba6d2732d394ac00defb9da9
Victor Victor RJ <wrjr04@gmail.com>
2023-02-17 22:03:28 -0500

    Add bike controller REST.

--- src/main/java/com/globomatics/bike/controllers/BikesController.java
+++ src/main/java/com/globomatics/bike/controllers/BikesController.java
@@ -1,4 +1,27 @@
 package com.globomatics.bike.controllers;
 
+import com.globomatics.bike.models.Bike;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/bikes")
 public class BikesController {
+    @GetMapping()
+    public List<Bike> list() {
+        return new ArrayList<>();
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.OK)
+    public void create(@RequestBody Bike bike) {
+    }
+
+    @GetMapping("/{id}")
+    public Bike get(@PathVariable("id") long id) {
+        return new Bike();
+    }
 }
